e6352b59a0fb4310da5d1277a5c3d761e87c5332
gabrielwann <gabriel.sy.wan@gmail.com>
2024-09-18 21:32:25 -0400

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+'''
+This is  
+a multiline
+comment 
+'''
+
+"""This is 
+also a 
+multline comment"""
+
+
+# print on one line
+print('hello world')
+
+ # print on many lines
+print(
+'''
+<p>this is a pargarpah</p>     
+      
+      
+      
+<p>this is another paragraph</p>     
+      
+'''
+)
---
+++ idle.py
@@ -0,0 +1,13 @@
+
+'''
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+3 + 4
+7
+my_var = "gabriel"
+print(my_var)
+gabriel
+my_var = "Ollie"
+print (my_var)
+Ollie
+'''

6c90fc084c576d1ea49b3f2726fe0f434da3c038
gabrielwann <gabriel.sy.wan@gmail.com>
2024-10-01 13:36:57 -0400

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,10 @@
+critters = ['turtle', 'lizard', 'salamander', 'frog']
+
+for critter in critters:
+    if critter == "salamander":
+        print(critter.title()) 
+    else:
+        print(critter.upper()) 
+
+        
+        

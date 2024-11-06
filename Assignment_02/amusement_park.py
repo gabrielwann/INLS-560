5b1935f83f16e95a119552e47259d7ba233da516
gabrielwann <gabriel.sy.wan@gmail.com>
2024-11-06 17:45:57 -0500

    Add files via upload

--- Assignment_02/amusement_park.py
+++ Assignment_02/amusement_park.py
@@ -7,7 +7,7 @@ elif age < 18:
 elif age > 60:
     print("Admission is $35.00")
 elif age > 100:
-    print("Admission is $0 and you get a free beer!")
+    print("Admission is $0 and you get a free beer!") # here is the bug, it is caused because both "age > 60" and "age > 100" are true, but because "age > 60" is read first, that is what the program does.
 else:
     print("Admission is $40.00")
     
